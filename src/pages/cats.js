8baf38a2db85bedd1dd9a8ea9872c4f0e63057a2
gabxyz <gabxyzdev@gmail.com>
2021-11-22 15:17:16 -0300

    add CatCard component and use queries to load content

--- src/pages/cats.js
+++ src/pages/cats.js
@@ -1,18 +1,38 @@
+import { graphql } from "gatsby"
 import React from "react"
+import CatCard from "../components/CatCard"
 import Layout from "../components/Layout"
+import * as style from "../styles/cats.module.css"
 
-export default function Cats() {
+export default function Cats({ data }) {
+  console.log(data)
+  const cats = data.allMarkdownRemark.nodes
   return (
     <Layout>
-      <div>
-        <h1>COOLCATS</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse odio sit
-          laboriosam distinctio minima eligendi veritatis quibusdam, saepe sunt,
-          quaerat reiciendis libero ut ex amet cum! Quas quasi fugiat
-          voluptates.
-        </p>
+      <div className={style.cats}>
+        <h1>COOL CATS</h1>
+        <div className="cats-container">
+          {cats.map(cat => (
+            <CatCard cats={cat} />
+          ))}
+        </div>
       </div>
     </Layout>
   )
 }
+
+// export page query
+export const Query = graphql`
+  query CatsPage {
+    allMarkdownRemark {
+      nodes {
+        frontmatter {
+          title
+          description
+          src
+        }
+        id
+      }
+    }
+  }
+`
