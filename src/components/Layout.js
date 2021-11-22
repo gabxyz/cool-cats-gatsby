08b6306730621f543afafc34bed2c73842a2f27f
gabxyz <gabxyzdev@gmail.com>
2021-11-22 15:13:50 -0300

    added the framer motion styles to the layout for page transitions

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,14 +1,37 @@
 import React from "react"
 import Navbar from "./Navbar"
 import "../styles/global.css"
+import { motion } from "framer-motion"
 
 export default function Layout({ children }) {
   return (
     <div className="layout">
       <Navbar />
-      <div className="content">{children}</div>
+      <motion.main
+        className="content"
+        initial={{
+          opacity: 0,
+          x: -200,
+        }}
+        animate={{
+          opacity: 1,
+          x: 0,
+        }}
+        exit={{
+          opacity: 0,
+          x: 200,
+        }}
+        transition={{
+          type: "spring",
+          mass: 0.35,
+          stiffness: 75,
+          duration: 0.3,
+        }}
+      >
+        {children}
+      </motion.main>
       <footer>
-        <p>Developed by gabxyz - 2021</p>
+        <p>Developed by Gabxyz - 2021</p>
       </footer>
     </div>
   )
