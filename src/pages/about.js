71586595d588ca67803443bdd095f65d4670ae26
gabxyz <gabxyzdev@gmail.com>
2021-11-22 15:15:27 -0300

    add some styles to the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Layout from "../components/Layout"
+import * as style from "../styles/about.module.css"
 
 export default function About() {
   return (
     <Layout>
-      <div>
+      <div className={style.about}>
         <h1>About this website</h1>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos totam
