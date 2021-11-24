259557705b801dbf16f8a4a51ebfc8a3a6c61898
gabxyz <gabxyzdev@gmail.com>
2021-11-24 14:10:41 -0300

    installed the plugins for optimized images

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,11 +8,21 @@ module.exports = {
   /* Your site config here */
   plugins: [
     "gatsby-transformer-remark",
+    "gatsby-plugin-image",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-sharp",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `cats`,
         path: `${__dirname}/src/cats/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
       },
     },
   ],
