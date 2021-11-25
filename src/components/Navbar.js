234c1a7e55854c2b9ec1998a21a652d1270b4ea9
gabxyz <gabxyzdev@gmail.com>
2021-11-25 16:07:17 -0300

    add styles to when the page is active

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,13 +5,21 @@ export default function Navbar() {
   return (
     <nav>
       <div className="logo">
-        <img src="/catLogo.svg" alt="Cat face silhouette"></img>
+        <Link to="/">
+          <img src="/catLogo.svg" alt="Cat face silhouette"></img>
+        </Link>
         <h1>Cool Cats</h1>
       </div>
       <div className="links">
-        <Link to="/">Home</Link>
-        <Link to="/cats">Cats</Link>
-        <Link to="/about">About</Link>
+        <Link to="/" activeStyle={{ color: "#e38b80" }}>
+          Home
+        </Link>
+        <Link to="/cats" activeStyle={{ color: "#e38b80" }}>
+          Cats
+        </Link>
+        <Link to="/about" activeStyle={{ color: "#e38b80" }}>
+          About
+        </Link>
       </div>
     </nav>
   )
